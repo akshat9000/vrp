70437c380bfafc97b9de7bbc199fd1644f1c0dc8
akshat9000 <srivastava.akshat9000@gmail>
2024-04-19 11:55:07 +0100

    migtated ipynb to py

--- helpers.py
+++ helpers.py
@@ -1,7 +1,6 @@
-import numpy as np
 import pandas as pd
-import datetime
 
+ID = 0
 
 def getMonthEnd(x: pd.Timestamp):
     return x + pd.offsets.BMonthEnd()
@@ -15,5 +14,31 @@ def getNextMonthBegin(x: pd.Timestamp):
     return x + pd.offsets.BMonthBegin()
 
 
-def getNextMonthEnd(x: pd.Timestamp):
-    return x + getNextMonthBegin(x)
+def get3MonthEnd(x: pd.Timestamp):
+    t = x + pd.offsets.MonthEnd(3)
+    if t.weekday() == 6:
+        return t - pd.Timedelta(2, unit='D')
+    elif t.weekday() == 5:
+        return t - pd.Timedelta(1, unit='D')
+    else:
+        return t
+
+
+def get4MonthBegin(x: pd.Timestamp):
+    t = x + pd.offsets.MonthBegin(3)
+    if t.weekday() == 6:
+        return t + pd.Timedelta(1, unit='D')
+    elif t.weekday() == 5:
+        return t + pd.Timedelta(2, unit='D')
+    else:
+        return t
+    
+
+def idGenerate():
+    ID += 1
+    return ID
+
+
+def resetID():
+    ID = 0
+    return None
--- vrp_strat.py
+++ vrp_strat.py
@@ -9,6 +9,9 @@ from helpers import *
 from params import netVal, cash_percentage
 
 
+
+"""
+v1
 optChain = pd.read_csv("optionChainData.csv")
 
 optChain["Date"] = pd.to_datetime(optChain["Date"], format="%d-%m-%Y")
@@ -18,7 +21,11 @@ optChain["Expiry_Date"] = pd.to_datetime(optChain["Expiry_Date"], format="%d-%m-
 dateList = [pd.Timestamp(item) for item in sorted(optChain["Date"].unique())]
 
 expiryDateList = [pd.Timestamp(item) for item in sorted(optChain["Expiry_Date"].unique())]
-
+"""
+# File now stored as HDF5 format
+optChain = pd.read_hdf("optionDataClean.h5")
+dateList = [item for item in sorted(optChain["Date"].unique())]
+expiryDateList = [item for item in sorted(optChain["Expiry_Date"].unique())]
 
 
 def getOrderBook():
@@ -26,6 +33,67 @@ def getOrderBook():
         tdf = optChain[optChain["Date"] == dt]
         tdf = tdf.reset_index().drop(columns="index")
         yield tdf
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+        return None
+    
+    def enqueue(self, item):
+        self.queue.append(item)
+        return None
+    
+    def dequeue(self, id=None):
+        if id == None:
+            if len(self.queue) > 0:
+                t = self.queue.pop(0)
+                return t
+            else:
+                raise Exception("Queue is Empty")
+        else:
+            _arg = None
+            for i in range(len(self.queue)):
+                if id == self.queue[i].id:
+                    _arg = self.queue.index(self.queue[i])  # Unnecessary but idc
+            if _arg is not None:
+                t = self.queue.pop(_arg)
+                return t
+            else:
+                raise Exception("ID doesn't exist in Queue")
+    
+    def peak(self, ln=1):
+        if len(self.queue) > 0:
+            return self.queue[:ln+1]
+        else:
+            raise Exception("Queue is Empty!")
+    
+
+    def items(self):
+        if len(self.queue) > 0:
+            for i in range(len(self.queue)):
+                yield self.queue[i]
+        else:
+            raise Exception("Queue is Empty!")
+        
+    def sortQueue(self):
+        return sorted(self.queue, key = lambda x: x.entry_time)
+
+
+
+class Straddle:
+    def __init__(self, _id, entry_time, expiry_time, strike, c_bid, c_ask, p_bid, p_ask, lots, option_size=100):
+        self.id = _id
+        self.entry_time = entry_time
+        self.expiry_time = expiry_time
+        self.strike = strike
+        self.c_bid = c_bid
+        self.c_ask = c_ask
+        self.p_bid = p_bid
+        self.p_ask = p_ask
+        self.lots = lots
+        self.trade_value = (c_bid + p_bid) * self.lots * option_size
+        return None
 
 
 
@@ -44,15 +112,20 @@ class VRP:
             self.call_range = 0
             self.put_range = 0
             self.entry_time = None
-            self.c_bid = None
-            self.p_bid = None
+            self.c_bid = 0
+            self.p_bid = 0
+            self.c_ask = 0
+            self.p_ask = 0
             self.call_delta = None
             self.put_delta = None
             self.pf_delta = None
-            self.end_of_month = pd.Timestamp(year=1900, month=1, day=1)
-            self.next_month_begin = pd.Timestamp(year=1900, month=1, day=1)
+            self.ids = []
+            self.q = Queue()
+            self.end_of_3month = pd.Timestamp(year=1900, month=1, day=1)
+            self.next_4month_begin = pd.Timestamp(year=1900, month=1, day=1)
             self.expiry_date = pd.Timestamp(year=1900, month=1, day=1)
             self.expiry_date_list = None
+            self.past_expiry_list = []
             self.exit_time = None
             self.entry_trades = pd.DataFrame()
             self.exit_trades = pd.DataFrame()
@@ -61,111 +134,72 @@ class VRP:
             print(f"ERROR IN INITIALIZING VRP CLASS: \n\t{str(e)}\n")
             traceback.print_exc()
             exit()
-        
         return None
     
 
-    def get_MonthEnd(self) -> None:
-        self.end_of_month = getMonthEnd(self.entry_time)
+    def get_3MonthEnd(self) -> None:
+        self.end_of_month = get3MonthEnd(self.entry_time)
         return None
     
 
-    def get_NextMonthBegin(self) -> None:
-        self.next_month_begin = getNextMonthBegin(self.entry_time)
+    def get_4MonthBegin(self) -> None:
+        self.next_4month_begin = get4MonthBegin(self.entry_time)
         return None
     
 
-    def getMonthExpiry(self):
-        # end_of_month = getMonthEnd(self.entry_time)
-
+    def get3MonthExpiry(self):
         for i in range(len(self.expiry_date_list)):
-            if self.end_of_month == self.expiry_date_list[i]:
+            if self.end_of_3month == self.expiry_date_list[i]:
                 return self.expiry_date_list[i]
-            elif self.end_of_month < self.expiry_date_list[i]:
+            elif self.end_of_3month < self.expiry_date_list[i]:
                 return self.expiry_date_list[i-1]
             else:
                 pass
-        
         return None
-    
-
-    def calculateDelta(self):
-        c_delta = self.reldf.at[self.reldf["Strike_Dist"].argmin(), "C_Delta"]
-        p_delta = self.reldf.at[self.reldf["Strike_Dist"].argmin(), "P_Delta"]
-
-        self.call_delta = self.current_lots * self.option_size * c_delta * (-1)
-        self.put_delta = self.current_lots * self.option_size * p_delta * (-1)
-
-        self.pf_delta = self.put_delta - self.call_delta
-        return None
-    
-
-    def hedge(self):
-        if self.pf_delta < 0:
-            buy_vix = self.pf_delta * self.reldf.at[self.reldf["Strike_Dist"].argmin(), "VIX_Close"]
-            self.portfolio -= buy_vix
-            return None
-        elif self.pf_delta > 0:
-            sell_vix = self.pf_delta * self.reldf.at[self.reldf["Strike_Dist"].argmin(), "VIX_Close"]
-            self.portfolio += sell_vix
-            return None
-        else:
-            return None
     
 
     def main(self) -> None:
         for idx, order_book in enumerate(getOrderBook()):
             self.entry_time = pd.Timestamp(order_book["Date"].unique()[0])
-            self.end_of_month = self.get_MonthEnd()
-            self.next_month_begin = self.get_NextMonthBegin()
-            self.expiry_date_list = sorted([pd.Timestamp(item) for item in order_book["Expiry_Date"].unique()])
-            self.expiry_date = self.getMonthExpiry()
+            self.end_of_3month = self.get_3MonthEnd()
+            self.next_4month_begin = self.get_4MonthBegin()
+            self.expiry_date_list = sorted(order_book["Expiry_Date"].unique())
+            self.expiry_date = self.get3MonthExpiry()
             self.reldf = order_book[order_book["Expiry_Date"] == self.expiry_date]
 
-            if self.entry_time < self.expiry_date and self.entry_time < self.end_of_month:
-                # TODO: ADD LOGIC FOR ENTRY AND DELTA CALCULATION + HEDGING
-                if not self.invested:
-                    self.strike = self.reldf.at[self.reldf["Strike_Dist"].argmin(), "Strike"]
-                    self.available_balance = int(self.portfolio * (1 - self.cash_percentage))
-                    # self.unused_balance = self.portfolio - self.available_balance
-                    self.c_bid = self.reldf.at[self.reldf["Strike_Dist"].argmin(), "C_Bid"]
-                    self.p_bid = self.reldf.at[self.reldf["Strike_Dist"].argmin(), "P_Bid"]
+            min_strike_dist = self.reldf['Strike_Dist'].argmin()
 
-                    self.call_range = self.strike + self.c_bid
-                    self.put_range = self.strike - self.p_bid
-                    # c_delta = self.reldf.at[self.reldf["Strike_Dist"].argmin(), "C_Delta"]
-                    # p_delta = self.reldf.at[self.reldf["Strike_Dist"].argmin(), "P_Delta"]
-                    self.current_lots = int((self.available_balance / (self.c_bid + self.p_bid)) / self.option_size)
-
-                    premium_collected = self.option_size * ((self.current_lots * self.c_bid) + (self.current_lots * self.p_bid))
-                    self.portfolio += premium_collected
-
-                    self.calculateDelta()
-                    self.hedge()
-
-                    self.invested = True
-                    
+            if self.expiry_date in self.past_expiry_list:
+                # TRAVERSE QUEUE, MARK TO MARKET FOR EACH, SQUARE OFF IF EXPIRY REACHED, IF INVESTED
+                if self.invested:
+                    for conts in self.q.items():
+                        s_expiry_time = self.entry_time
+                        if s_expiry_time < self.entry_time:
+                            # TODO: ADD LOGIC TO DEQUEUE USING ID -> Done
+                            t = self.q.dequeue(conts.id)
+                            continue    # Continue or Pass?
+                        else:
+                            if s_expiry_time == self.entry_time:
+                                # SQUARE OFF POSITION, MARK TO MARKET, HEDGE, DEQUEUE CONTRACT
+                                pass
+                            else:
+                                # MARK TO MARKET, HEDGE
+                                pass
                 else:
-                    self.calculateDelta()
-                    self.hedge()
-            elif self.entry_time == self.expiry_date:
-                # TODO: ADD LOGIC FOR SQUARING OFF
-                vix_close = self.reldf.at[0, "VIX_Close"]
-                if vix_close > self.call_range:
-                    # CALL EXERCISED -> CALCULATE LOSS + HEDGE
-                    self.invested = False
-                elif vix_close < self.put_range:
-                    # PUT EXERCISED -> CALCULATE LOSS + HEDGE
-                    self.invested = False
-                else:
-                    # DO NOTHING -> PREMIUM ALREADY COLLECTED
-                    self.invested = False
-            elif self.expiry_date < self.entry_time <= self.end_of_month:
-                # PORTFOLIO IDLE
-                pass
+                    # MARK TO MARKET OTHER TRADES OR SIT IDLE
+                    pass
             else:
-                # CONDITION NEVER REACHED
-                pass
+                self.past_expiry_list.append(self.expiry_date)
+                # LOGIC TO SELL STRADDLE AND HEDGE FOR FIRST TIME EXPIRY DATE
+                strike = self.reldf.at[min_strike_dist, 'Strike']
+                c_bid = self.reldf.at[min_strike_dist, 'C_Bid']
+                c_ask = self.reldf.at[min_strike_dist, 'C_Ask']
+                p_bid = self.reldf.at[min_strike_dist, 'P_Bid']
+                p_ask = self.reldf.at[min_strike_dist, 'P_Ask']
+                c_delta = self.reldf.at[min_strike_dist, 'C_Delta']
+                p_delta = self.reldf.at[min_strike_dist, 'P_Delta']
+                
+
 
 
 
